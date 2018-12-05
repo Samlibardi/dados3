48d15cdee53f289dfe666a408eb29b0106eebf67
du-santini <dupradosantini@gmail.com>
2018-12-05 21:25:26 -0200

    Revert "Update ordenaInterna.h"
    This reverts commit 18fa2e953b927d787faa235d5219d04b9f0bfc02.

--- ordenaInterna.h
+++ ordenaInterna.h
@@ -1,6 +1,3 @@
 #pragma once
 
-int ordenaInterna(char* srcname, char* destname);
-
-//srcname é o arquivo de origem
-//destname é o arquivo de destino, onde os dados serão escritos.
+int ordenaInterna(char* filename);
