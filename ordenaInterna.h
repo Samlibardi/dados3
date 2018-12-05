18fa2e953b927d787faa235d5219d04b9f0bfc02
du-santini <dupradosantini@gmail.com>
2018-12-05 21:10:18 -0200

    Update ordenaInterna.h

--- ordenaInterna.h
+++ ordenaInterna.h
@@ -1,3 +1,6 @@
 #pragma once
 
-int ordenaInterna(char* filename);
+int ordenaInterna(char* srcname, char* destname);
+
+//srcname é o arquivo de origem
+//destname é o arquivo de destino, onde os dados serão escritos.
