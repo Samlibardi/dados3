30ae68b830bb93a06175765e40b8ac1889396d40
Samuel Libardi Godoy <samlibardi@outlook.com>
2018-12-06 21:41:33 -0200

    Melhor uso de memória, documentação
    Documentadas as funções split e ordenaExterna

--- ordenaExterna.c
+++ ordenaExterna.c
@@ -1,52 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include "asprintf.h"
+#include "merge.h"
 
 #define MAX_OPEN_FILES 256
-
 
 int ordenaExterna(char* srcname, char* destname) {
 	int splitc;
 	int err;
 
-	if (splitc = split(srcname)) {
+	char** splitv = NULL;
+
+	//Quebra o arquivo e ordena internamente.
+	if (split(srcname, &splitv, &splitc)) {
 		int destnamelen;
-		char** splitv = malloc(sizeof(char*) * splitc);
-		for (int i = 0; i < splitc; i++) {
-			char* destname = malloc(destnamelen = (strlen(srcname) + 22) * sizeof(char));
-			sprintf(destname, "sub_%d_%s", i, srcname);
-			splitv[i] = destname;
-			ordenaInterna(destname, destname);
-		}
 
 		char **srcsubv = splitv, **destsubv;
 		int srcsubc = splitc, destsubc, totalsubs = splitc;
 
+		//Para cada nivel, sao abertos MAX_OPEN_FILES de cada vez, e mergidos em 1.
+		//No total, em cada nivel, existem srcsbubc subarquivos de origem e srcsubc/MAX_OPEN_FILES subarquivos de destino
+		//Isso se repete ate que apenas 1 subarquivo destino seja escrito (arquivo final)
 		do {
+			//Calcula quantos os arquivos de destino e aloca espaço
 			destsubc = ceilf(srcsubc / (float)MAX_OPEN_FILES);
 			destsubv = malloc(sizeof(char*) * destsubc);
+
+			//Faz o merge de um bloco de arquivos de origem.
 			for (int i = 0; i < destsubc; i++, totalsubs++) {
-				char* destname = destsubv[i] = malloc(22 * sizeof(char));
-				sprintf(destname, "sub_%d_%s", totalsubs, srcname);
-				if (err = merge(min(256, srcsubc - i * MAX_OPEN_FILES), srcsubv + i * MAX_OPEN_FILES, destname))
+				char* destname;
+				asprintf(&destname, "sub_%d_%s", totalsubs, srcname);
+				destsubv[i] = destname;
+				if (err = merge((int)fmin(MAX_OPEN_FILES, srcsubc - i * MAX_OPEN_FILES), srcsubv + i * MAX_OPEN_FILES, destname))
 					return err;
 			}
-			for (int i = 0; i < srcsubc; i++) free(srcsubv[i]);
+			
+			//Libera o espaço utilizado pela lista de subarquivos de origem
+			for (int i = 0; i < srcsubc; i++) {
+				free(srcsubv[i]);
+				srcsubv[i] = NULL;
+			};
 			free(srcsubv);
-			srcsubv = destsubv;
 
+			// A lista de subarquivos de origem é agora a lista de subarquivos destino que acabaram de ser escritos
+			srcsubv = destsubv;
 			srcsubc = destsubc;
+			
+			// Repete essa operação até que se escreva apenas o arquivo final
 		} while (destsubc > 1);
 
+		// Renomeia o ultimo subarquivo gerado pelo nome de destino
 		remove(destname);
 		rename(destsubv[0], destname);
 
-		//for (int i = 0; i < splitc; i += 256) {
-		//	char* destname = malloc(20);
-		//	sprintf(destname, "sub_s%s.part%d", srcname, i);
-		//	if (err = merge(min(256, splitc - i), splitv + i, destname))
-		//		return err;
-		//}
 		return 0;
 	}
 }
--- split.c
+++ split.c
@@ -2,24 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 #include "registro.h"
+#include "compares.h"
 
-#define SPLIT_SIZE 64000
-#define SPLIT_REC_COUNT (SPLIT_SIZE - 4)/sizeof(registro)
+//TODO: verificar se o split está de acordo com as especificações.
+#define SPLIT_SIZE 64000									// Tamanho de cada subarquivo
+#define SPLIT_REC_COUNT (SPLIT_SIZE - 4)/sizeof(registro)	// Numero de registros em um subarquivo
 
 int status;
 
-int split(char* filename) {
+int split(char* filename, char*** sub_filenamesv, int* sub_filenamesc) {
 	int subc = 0, readc, destnamelen;
+
+	//Abre o arquivo origem e verifica o cabeçalho
 	FILE* fsrc = fopen(filename, "rb"), *fdest;
 
 	fread(&status, 4, 1, fsrc);
-	
-	char* destname = malloc(destnamelen = (strlen(filename) + 22) * sizeof(char));
 
+	//Buffer para o conteudo de um subarquivo, completo
 	void* fbuffer = malloc(SPLIT_SIZE);
 
+	*sub_filenamesv = NULL;
+
+	//enquanto for possivel ler registros do arquivo original, le, ordena internamente e escreve o subarquivo
 	while (readc = fread(fbuffer, sizeof(registro), SPLIT_REC_COUNT, fsrc)) {
-		sprintf(destname, "sub_%d_%s", subc, filename);
+		//Ordena o buffer por quicksort
+		qsort(fbuffer, readc, sizeof(registro), regcmp);
+		//Formata o nome de saida do arquivo
+		char* destname;
+		asprintf(&destname, "sub_%d_%s", subc, filename);
+		//Salva o nome do subarquivo criado
+		*sub_filenamesv = realloc(*sub_filenamesv, sizeof(char*)* (subc + 1));
+		(*sub_filenamesv)[subc] = destname;
+		//Escreve o cabeçalho e o buffer ordenado em disco
 		fdest = fopen(destname,"wb");
 		status = 0;
 		fwrite(&status, 4, 1, fdest);
@@ -27,17 +41,16 @@ int split(char* filename) {
 		rewind(fdest);
 		status = 1;
 		fwrite(&status, 4, 1, fdest);
+		//Fecha o arquivo e aumenta a contagem de subarquivos escritos
 		fclose(fdest);
 		subc++;
-		if (readc < SPLIT_REC_COUNT) break;
 	}
+	
+	//Desaloca o buffer
+	free(fbuffer);
 
-	char** subnames = malloc(sizeof(char*) * subc);
-	/*for (int i = 0; i < subc; i++) {
-		subnames[i] = malloc(destnamelen);
-		strcpy(subnames[i], destname);
-		subnames[i][destnamelen - 1] = '0' + i;
-	}*/
+	//Retorna o numero de subarquivos gerados
+	*sub_filenamesc = subc;
 
 	return subc;
 }
